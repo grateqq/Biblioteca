eef9c584a8a15b62a0c32eee15ebaf98816d5017
grateqq <156215485+grateqq@users.noreply.github.com>
2025-02-17 00:08:51 -0300

    f-showlib

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+console.log("hola")
+const  myLibrary =[]
 
-
-function Libro (name, autor, pages, year, read) {
+function Book (name, autor, pages, year, read) {
   this.name= name,
   this.autor= autor,
   this.pages= pages,
@@ -8,4 +9,36 @@ function Libro (name, autor, pages, year, read) {
   this.read= read
 };
 
-const libro1 = Libro ("The Hoobit", "RRRKing", 100, 1988, false );
+// const libro1 = new Libro ("The Hoobit", "RRRKing", 100, 1988, false );
+// library.push(libro1)
+// console.log(libro1)
+// console.log(library.length)
+
+function addBookToLibrary (name, autor, pages, year, read)  {
+  const libros1 = new Book (name, autor, pages, year, read);
+  // console.log(libro1)
+  myLibrary.push(libros1)
+}
+
+addBookToLibrary ("The Hoobit", "RRRKing", 100, 1988, false );
+addBookToLibrary ("The Hoobit", "RRRKing", 100, 1988, false );
+addBookToLibrary ("The Hoobit", "RRRKing", 100, 1988, false );
+addBookToLibrary ("The Hoobit", "RRRKing", 100, 1988, false );
+console.log(myLibrary)
+
+
+function showMyLibrary (){
+  //opcion 1: for
+  // for (i=1; i<myLibrary.length; i++) {
+  //   console.log (myLibrary[i])
+  // }
+  
+  //opcion 2: map
+  // myLibrary.map((libro)=> console.log(libro))
+
+  //opcion 3:firEach
+  console.log("opcion 3")
+  myLibrary.forEach((value)=>console.log(value))
+}
+
+showMyLibrary ()
