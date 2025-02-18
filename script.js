0fc658a99a71fb29c3ecf79f298df05082aa2af2
grateqq <156215485+grateqq@users.noreply.github.com>
2025-02-18 02:56:38 -0300

    create new js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log("hola")
 const  myLibrary =[]
 
 function Book (name, autor, pages, year, read) {
@@ -9,36 +8,79 @@ function Book (name, autor, pages, year, read) {
   this.read= read
 };
 
-// const libro1 = new Libro ("The Hoobit", "RRRKing", 100, 1988, false );
-// library.push(libro1)
-// console.log(libro1)
-// console.log(library.length)
-
 function addBookToLibrary (name, autor, pages, year, read)  {
   const libros1 = new Book (name, autor, pages, year, read);
-  // console.log(libro1)
   myLibrary.push(libros1)
 }
 
-addBookToLibrary ("The Hoobit", "RRRKing", 100, 1988, false );
-addBookToLibrary ("The Hoobit", "RRRKing", 100, 1988, false );
-addBookToLibrary ("The Hoobit", "RRRKing", 100, 1988, false );
-addBookToLibrary ("The Hoobit", "RRRKing", 100, 1988, false );
+addBookToLibrary("Harry Potter", "J. J. Rowling", 100, 1997, true)
+addBookToLibrary("The Lord of the Rings", "Tolkien",1000, 1954, true)
+addBookToLibrary("The Islan", "John Smit", 102, 1992, false)
+addBookToLibrary("Silo", "Hugh Howey", 250, 2013, false)
+
 console.log(myLibrary)
 
-
 function showMyLibrary (){
-  //opcion 1: for
-  // for (i=1; i<myLibrary.length; i++) {
-  //   console.log (myLibrary[i])
-  // }
+  myLibrary.forEach((value,index)=>{
+    // const contenedor = document.getElementById("container")
+
+    // contenedor.innerHTML = '<div class"libro"><p class="name"></p><p class="autor"></p><p class="pages"></p><p class="year"></p><p class="read"></p><div>' 
+    // const name = document.querySelector(".name")
+    // const autor = document.querySelector(".autor")
+    // const pages = document.querySelector(".pages")
+    // const year = document.querySelector(".year")
+    // const read = document.querySelector(".read")
   
-  //opcion 2: map
-  // myLibrary.map((libro)=> console.log(libro))
+    // name.innerHTML = myLibrary[value].name
+    // autor.innerHTML = myLibrary[value].autor
+    // pages.innerHTML = myLibrary[value].pages
+    console.log("value: ")
+    console.log(value) // pasa por cada elemento objeto dela array
+    //harrypotener
+    let library = document.getElementById("container")
+    console.log(library)
 
-  //opcion 3:firEach
-  console.log("opcion 3")
-  myLibrary.forEach((value)=>console.log(value))
+    let newDiv = document.createElement("div")
+    newDiv.classList.add("libro");
+
+    let newname = document.createElement("h4")
+    newname.innerHTML = myLibrary[index].name
+    newDiv.appendChild(newname)
+
+    let newautor = document.createElement("p")
+    newautor .innerHTML = myLibrary[index].autor
+    newDiv.appendChild(newautor)
+
+    let newpages = document.createElement("p")
+    newpages.innerHTML = myLibrary[index].pages
+    newDiv.appendChild(newpages)
+
+    let newyear = document.createElement("p")
+    newyear.innerHTML = myLibrary[index].year
+    newDiv.appendChild(newyear)
+    
+    let bookstatus
+    if (myLibrary[index].read) { bookstatus = "already read"}
+    else {bookstatus = "not read yet"}
+
+    let newread = document.createElement("p")
+    newread.innerHTML = bookstatus
+    newDiv.appendChild(newread)
+    
+  
+    console.log(newDiv)
+
+
+
+
+     
+    
+    
+    
+    library.appendChild(newDiv)
+    
+
+
+})
 }
-
 showMyLibrary ()
