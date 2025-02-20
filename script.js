a910bf32ac28e057a27cec07c935f526d2dbde4b
grateqq <156215485+grateqq@users.noreply.github.com>
2025-02-20 00:44:40 -0300

    nose

--- script.js
+++ script.js
@@ -7,16 +7,14 @@ function Book (title, autor, pages, year, read) {
   this.year= year,
   this.read= read,
   this.status = function() {return this.read ?"already read" : "not read yet";}
- }
-
-
-
+}
 
 function addBookToLibrary (title, author, pages, year, read)  {
   const libros1 = new Book (title, author, pages, year, read);
   myLibrary.push(libros1)
 }
 
+//ejemplos
 addBookToLibrary("Harry Potter", "J. J. Rowling", 100, 1997, true)
 addBookToLibrary("The Lord of the Rings", "Tolkien",1000, 1954, true)
 addBookToLibrary("The Islan", "John Smit", 102, 1992, false)
@@ -28,14 +26,17 @@ console.log(myLibrary)// libro harry, the lord... , the islan y silo.
 function showMyLibrary () {
   myLibrary.forEach((value)=>{
     let containerlibrery = document.getElementById("container");
-    containerlibrery.innerHTML = `
-    <div class="libro">
+    let containerbook = document.createElement("div")
+    containerbook.classList.add("libro")
+
+    containerlibrery.appendChild(containerbook)
+   
+    containerbook.innerHTML = `
       <h2>${value.title}</h2>
       <p>Author: ${value.author}</p>
       <p>Pages: ${value.pages}</p>
       <p>Year: ${value.year}</p>
       <p>Read?: ${value.status()}</p>
-    </div
     `;
     console.log(containerlibrery)
   })
